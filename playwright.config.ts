1336f801384e7bd58d3b25700fe686c1cdd22684
Qinqin Cheng <QCheng@toshiba-tap.com>
2024-06-05 15:20:08 +1000

    Add Allure reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,12 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html',
-  reporter: [['html', { outputFolder: 'playwright-report' }], ['line']],
+  //reporter: [['html', { outputFolder: 'playwright-report' }], ['line']],
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }],
+    ['allure-playwright', {outputFolder: 'allure-results'}]
+  ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -51,14 +56,14 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
 
     /* Test against branded browsers. */
     // {
